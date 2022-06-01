907198ccf6d13c349aa0ab6827e2f1950c2eee1a
「 LovelyPrince™ 」 <90893568+Awesome-Prince@users.noreply.github.com>
2022-06-01 20:57:29 +0530

    Update hacking_script.py

--- NekoRobot/hacking_script.py
+++ NekoRobot/hacking_script.py
@@ -1,13 +1,13 @@
 PM_START_TEXT = """
-────「 [{}](https://telegra.ph/file/bdc771a084e2c806ec756.jpg) 」────
-*やあ! {} My Darling, Iam your SweetHeart💜,
-I am an Anime themed advance group management bot with a lot of Sexy Features.*
+────「 [{}](https://telegra.ph/file/bdbbe3693be14f6c2cce7.jpg) 」────
+*やあ! {} My Darling, Iam your DeadlyHeart🖤,
+I am an Anime themed Deadly group management bot with a lot of Devil Features.*
 ➖➖➖➖➖➖➖➖➖➖➖➖➖
 ◈ *Uptime:* `{}`
 ◈ `{}` *users, across* `{}` *chats.*
 ◈ *Thanks To @NekoXRobot For NekoRobot Repo*
 ➖➖➖➖➖➖➖➖➖➖➖➖➖
-➢ Try The Help Buttons Below To Know My Abilities ♡
+➢ Try The Help Buttons Below To Know My Abilities ✺
 """
 
 
